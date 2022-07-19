9db3717f3d3219b4cbedd7a9ea166c1b7c7fac63
Florian Appel <florian.appel1@gmail.com>
2022-07-19 07:59:25 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FotterComponent } from './fotter/fotter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FotterComponent
   ],
   imports: [
     BrowserModule,
